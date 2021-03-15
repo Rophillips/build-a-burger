461ca80f16bbae88cc30f0e9bfe8efc9b505d8e4
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-15 11:12:03 -0400

    establishing connection to db

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,22 @@
 // set up code to connect Node to Mysql
-// export the connection
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Eddieluna25!",
+    database: "burgers_db"
+});
+
+//  make the connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// export connection to ORM
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,18 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 -- Create the burgers_db.
+CREATE DATABASE burgers_db;
 -- Switch to or use the burgers_db.
+USE burgers_db;
 -- Create a burgers table with these fields:
---    id: an auto incrementing int that serves as the primary key.
---    burger_name: a string.
---    devoured: a boolean.
+CREATE TABLE burgers {
+    -- id: an auto incrementing int 
+    id INT NOT NULL AUTO_INCREMENT,
+    --    burger_name: a string.
+    burger_name VARCHAR(100),
+    --    devoured: a boolean.
+    devoured BOOLEAN DEFAULT false,
+    -- setting id as primary key
+    PRIMARY KEY (id)
+};
+
