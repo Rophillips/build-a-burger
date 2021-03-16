e9bd78cfa65038c8b96c46f03e1d8f7a1c7e6612
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-16 14:22:15 -0400

    working on orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,77 @@
 // Import (require) connection.js into orm.js
 const connection = require("../config/connection.js");
 
-// Create the methods that will execute the necessary MySQL commands in the controllers. 
 
-// selectAll()
+// Helper function for SQL syntax.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+     var arr = [];
+  
+        for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+  
+     return arr.toString();
+       }
+
+  
+
+  
+// Create the methods that will execute the necessary MySQL commands in the controllers. 
+// Methods needed in order to retrieve and store data in db
+var orm = {
+    // selectAll()
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+
+// function to insert table entry
 // insertOne()
-// updateOne()
-// Export the ORM object in module.exports.
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+            throw err;
+        }
+        cb(result);
+    });
+
+  },
+  // Function to update table entry
+  // updateOne()
+  updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+  }
+
+
+}
+
+
+
+
+
+
+
+// Export the orm object for the model
+module.exports = orm;
+
+
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,23 @@
 // Inside burger.js, import orm.js into burger.js
-
+var orm = require("../config/orm.js");
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Export at the end of the burger.js file
+module.exports = burger;
